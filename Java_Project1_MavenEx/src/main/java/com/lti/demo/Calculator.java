c5f90ffd90f206c78ddb28f8b0e3a926ad41c954
Amay Chugh <amaychugh20@gnail.com>
2021-07-28 15:51:35 +0530

    New branch commit

---
+++ Java_Project1_MavenEx/src/main/java/com/lti/demo/Calculator.java
@@ -0,0 +1,9 @@
+package com.lti.demo;
+
+public class Calculator {
+	public int sum(int x, int y) {
+		int result = x+y;
+		System.out.println("Sum Method");
+		return result;
+	}
+}
---
+++ Java_Project1_MavenEx/src/main/java/com/lti/demo/UserApp.java
@@ -0,0 +1,17 @@
+package com.lti.demo;
+
+public class UserApp {
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		int r = c.sum(100,200);
+		System.out.println(r);
+		
+		Product p = new Product(101,"Mobile",3000);
+		System.out.println(p);
+		
+		ProductService ps = new ProductService();
+		String str= ps.addProduct(p);
+	}
+	
+	
+}
